6b0da24e873b7dd9be85470898054a72cb8e4e62
Federico García <fede@divinodi.com>
2021-12-03 11:48:19 -0300

    Observer, class based.

--- observer/Observer.ts
+++ observer/Observer.ts
@@ -1,23 +1,17 @@
 type Listener<EventType> = (event: EventType) => void;
-type Observer<EventType> = {
-  subscribe: (listener: Listener<EventType>) => () => void;
-  publish: (event: EventType) => void;
-};
+class Observer<EventType> {
+  listeners: Listener<EventType>[] = [];
 
-function createObserver<EventType>(): Observer<EventType> {
-  let listeners: Listener<EventType>[] = [];
+  subscribe(listener: Listener<EventType>): () => void {
+    this.listeners.push(listener);
+    return () => {
+      this.listeners = this.listeners.filter(l => l !== listener);
+    };
+  }
 
-  return {
-    subscribe: (listener: Listener<EventType>): (() => void) => {
-      listeners.push(listener);
-      return () => {
-        listeners = listeners.filter(l => l !== listener);
-      };
-    },
-    publish: (event: EventType) => {
-      listeners.forEach(listener => listener(event));
-    },
-  };
+  publish(event: EventType) {
+    this.listeners.forEach(listener => listener(event));
+  }
 }
 
 const myListener: Listener<string> = event => {
@@ -27,7 +21,7 @@ const myListener2: Listener<string> = event => {
   console.log(`Received event, listener 2: ${event}`);
 };
 
-const myObserver = createObserver<string>();
+const myObserver = new Observer<string>();
 const unsubscribe = myObserver.subscribe(myListener);
 
 myObserver.publish('Hello 1');
